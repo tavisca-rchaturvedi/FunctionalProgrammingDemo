a43e48e30d55bb2ffa7ced4efa6a7b1fe095d734
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-08-08 14:56:52 +0530

    Threading Implemented.

--- src/main/java/lambda/ThreadExample.java
+++ src/main/java/lambda/ThreadExample.java
@@ -1,11 +1,15 @@
 package lambda;
 
 public class ThreadExample {
+
     public static void main(String[] args) {
         ThreadExample threadExample = new ThreadExample();
-        threadExample.numberPrinter();
-        threadExample.numberPrinter();
-        threadExample.numberPrinter();
+        Thread t1 = new Thread(() -> threadExample.numberPrinter());
+        t1.start();
+        Thread t2 = new Thread(threadExample::numberPrinter);
+        t2.start();
+        Thread t3 = new Thread(() -> threadExample.numberPrinter());
+        t3.start();
     }
 
     void numberPrinter(){
