ecefe2f11595de43c9b765df9a549ea495a6e334
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-08-08 14:43:32 +0530

    Solution Written using Optional(ifPresent method and passing consumer to it).

--- src/main/java/optionalExample/NameReader.java
+++ src/main/java/optionalExample/NameReader.java
@@ -6,9 +6,8 @@ public class NameReader {
 
     public static void main(String[] args) {
         NameReader nameReader = new NameReader();
-        Optional<String> name = nameReader.getName(1);
-        if(name.isPresent())
-            System.out.println(name.get());
+        Optional<String> mayBeName = nameReader.getName(1);
+        mayBeName.ifPresent(System.out::println);
     }
 
     public Optional<String> getName(int id){
