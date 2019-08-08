063643435608bc95cbbe4cbd4b480d182125d7d4
Rishabh Chaturvedi <rchaturvedi@tavisca.in>
2019-08-08 16:10:18 +0530

    Bringing Setup Files. Threads again working.

--- src/main/java/lambda/ThreadExample.java
+++ src/main/java/lambda/ThreadExample.java
@@ -1,20 +1,32 @@
 package lambda;
 
+import java.lang.reflect.Method;
+import java.util.Optional;
+
 public class ThreadExample {
 
     public static void main(String[] args) {
         ThreadExample threadExample = new ThreadExample();
         Thread t1 = new Thread(() -> threadExample.numberPrinter());
         t1.start();
+        System.out.println("Printed First");
         Thread t2 = new Thread(threadExample::numberPrinter);
         t2.start();
-        Thread t3 = new Thread(() -> threadExample.numberPrinter());
+        System.out.println("Printed Second");
+
+        Thread t3 = new Thread(threadExample::numberPrinter);
         t3.start();
+        System.out.println("Printing Third");
+
     }
 
+
+
     void numberPrinter(){
+        System.out.println(Thread.currentThread().getId() + " is running now");
         for(int i = 0; i < 100; i++){
             System.out.println("\t"+i);
         }
+        System.out.println(Thread.currentThread().getId() + " is stopped now");
     }
 }
